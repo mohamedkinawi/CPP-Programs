910150c35d6f92ed80a37f9b0e61adcd9f674dba
mohamedkinawi <98316626+mohamedkinawi@users.noreply.github.com>
2022-01-26 21:31:54 +0200

    Add files via upload

---
+++ Random/main.cpp
@@ -0,0 +1,38 @@
+// compile this file in command line "g++ -c main.cpp"
+// link and produce executable "g++ random.o main.o -o executableName"
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+#include "random.h"
+
+
+
+int main(){
+	
+	//my_random::set_random_seed(1); //for debugging only, otherwise automatically random with each program run
+	
+	const int flips = 1000;
+	int heads = 0;
+	int tails = 0;
+	for(int i=1;i<=flips;i++){
+		if(my_random::get_random_chance(0.5)==true) heads++;
+		else tails++;
+	}
+	cout<<"\nOut of "<<flips<<" flips:"<<endl;
+	cout<<"Heads occurred "<<heads<<" times\n";
+	cout<<"Tails occurred "<<tails<<" times\n\n";
+
+	const int diceMax = 6;
+	const int rolls = 1000000;
+	int diceResults[diceMax] = {};
+	for(int i=1;i<=rolls;i++){
+		diceResults[my_random::get_random_int(1,diceMax)-1]++;
+	}
+	cout<<"Out of "<<rolls<<" rolls:"<<endl;
+	for(int i=0;i<diceMax;i++){
+		cout<<i+1<<" occurred "<<diceResults[i]<<" times"<<endl;
+	}
+
+	return 0;
+}
---
+++ Random/random.cpp
@@ -0,0 +1,62 @@
+// compile this file in command line "g++ -c random.cpp"
+
+#include <ctime>
+#include <cstdlib>
+#include "random.h"
+
+namespace my_random {
+	void set_random_seed(int seed){
+		static bool seed_set = false;
+		if(!seed_set){
+			srand(seed);
+			seed_set = true;
+		}
+	}
+	void set_random_seed(){
+		set_random_seed(time(0));
+	}
+	int get_random_int(int lower , int upper){
+		set_random_seed();
+		double r = static_cast<double>(rand())/RAND_MAX;				// [0,1] double
+		int ans = static_cast<int>(lower + r*(static_cast<double>(upper)-lower+1));	// [lower,upper+1] int
+		if(ans==static_cast<double>(upper)+1)
+			return get_random_int(lower,upper);
+		else return ans;
+	}
+	double get_random_real(double lower, double upper){
+		set_random_seed();
+		double r = static_cast<double>(rand())/RAND_MAX;	// [0,1] double
+		double diff = r*(upper-lower);				// [0,upper-lower] double
+		return lower + diff;					// [lower,upper] double
+	}
+	bool get_random_chance(double prob){
+		set_random_seed();
+		if(prob>=1) return true;
+		else if(prob<=0) return false;
+		else return (static_cast<double>(rand())/RAND_MAX) < prob;
+		// if prob is 0.5 and static_cast<double>(rand())/RAND_MAX returned exactly 0.5 , should i roll again??
+	}
+}
+
+
+
+// NOTES TO MYSELF
+
+/*
+if we want to store some values that are associated with our random generator , we can make a class to encapsulate those values, but use
+a singleton pattern to allow instantiating only one object , since we can seed only one generator in the cstdlib per running program.
+*/
+
+/*
+OR we could use static global variables in the implementation file to store the values
+*/
+
+/*
+OR we could use static local variables inside the function implementations but we have to choose which function contains them because this is the only function
+that has access to these variables.
+*/
+
+/*
+In this implementation, no numbers are stored anywhere, i chose to make the numbers as inputs to functions, so everytime a random number is desired, the client must
+always give the bounds as inputs to the functions
+*/
